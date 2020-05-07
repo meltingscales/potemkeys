c7e3ddb0300a364819296e6f253ea6d80d01d933
Henry Post <henryfbp@gmail.com>
2020-05-07 15:44:06 -0500

    add drink-coffee

--- lisp/oop/human.lisp
+++ lisp/oop/human.lisp
@@ -1,11 +1,11 @@
 (defclass Person ()
   (
-    (name       
+    (name
       :initarg :name
       :initform "Joe Schmoe"
       :type string)
-    
-    (happiness  
+
+    (happiness
       :initarg :happiness
       :initform 100
       :type integer)
@@ -17,21 +17,43 @@
   )
 )
 
-(defmethod go-to-sleep ((p Person))
-  (setf (slot-value p 'energy) 100)
-  (format t "~S falls asleep. zzz..." (slot-value p 'name))
+(defmethod go-to-sleep ((obj Person))
+  ; sleeping restores energy
+  (setf (slot-value obj 'energy) 100)
+
+  ; if they are an office worker, they lose their caffeine from sleeping
+  (if (string-equal 'OfficeWorker (type-of obj))
+    (setf (slot-value obj 'caffeine) 0))
+
+  (format t "~s falls asleep. zzz...~%" (slot-value obj 'name))
 )
 
 
-;(defclass OfficeWorker (Person)
-;  (
-;
-;  )
-;)
+(defclass OfficeWorker (Person)
+  (
+    (caffeine
+      :initarg :caffeine
+      :initform 100
+      :type integer)
+  )
+)
 
-(defvar henry (make-instance 'Person :name "Henry"  :energy 0))
-(describe henry)
+(defmethod drink-coffee ((obj OfficeWorker))
+  (setf (slot-value obj 'caffeine) 100)
+
+  (format t "~s drinks a hot cup of joe. Mmm!~%" (slot-value obj 'name))
+)
 
 
-(go-to-sleep henry)
+(defvar henry 
+  (make-instance 'OfficeWorker 
+    :name "Henry"
+    :energy 0 ; needs to sleep :(
+    :caffeine 50
+  )
+)
+
+(go-to-sleep henry)   ; energy set to 100
+(drink-coffee henry)  ; caffeine set to 100
+
 (describe henry)
