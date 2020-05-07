8a26b728e8d41f6e384f8c3290f404be949c2a1e
Henry Post <henryfbp@gmail.com>
2020-05-07 14:42:29 -0500

    idk why setf doesnt work

--- lisp/oop/human.lisp
+++ lisp/oop/human.lisp
@@ -1,12 +1,24 @@
 (defclass Person ()
   (
-    (name       :initarg :name      :initform "Joe Schmoe"    :type string)
-    (happiness  :initarg :happiness :initform 0               :type integer)
-    (blue       :initarg :blue      :initform 0               :type integer)
+    (name       
+      :initarg :name
+      :initform "Joe Schmoe"
+      :type string)
+    
+    (happiness  
+      :initarg :happiness
+      :initform 100
+      :type integer)
+
+    (energy
+      :initarg :energy
+      :initform 100
+      :type integer)
   )
 )
 
 (defmethod go-to-sleep ((p Person))
+  (setf (slot-value p 'energy) 100) ;Why is energy not set to 100 here?!
   (format t "~S falls asleep. zzz..." (slot-value p 'name))
 )
 
@@ -17,7 +29,8 @@
 ;  )
 ;)
 
-(defvar henry (make-instance 'Person :name "Henry Post"  :happiness 100  :blue 0 ))
+(defvar henry (make-instance 'Person :name "Henry"  :energy 0))
+;(setf (slot-value henry 'energy) 100) ; Why does setf work here but not in go-to-sleep??
 
 (describe henry)
 (go-to-sleep henry)
