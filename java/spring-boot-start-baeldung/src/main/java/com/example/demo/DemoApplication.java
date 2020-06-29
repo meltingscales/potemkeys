054eb421d74405360fef6438e2ce74a87d1aa082
Henry Post <henryfbp@gmail.com>
2020-06-29 13:06:42 -0500

    broken but added wip

--- java/spring-boot-start-baeldung/src/main/java/com/example/demo/DemoApplication.java
+++ java/spring-boot-start-baeldung/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.example.demo.repository")
+@EntityScan("com.example.demo.model")
 @SpringBootApplication
 public class DemoApplication {
 
--- java/spring-boot-start-baeldung/src/main/java/com/example/demo/controller/SimpleController.java
+++ java/spring-boot-start-baeldung/src/main/java/com/example/demo/controller/SimpleController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
