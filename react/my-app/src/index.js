e8aa550e2aaa7c6e8bbdf67f93b6eb6d5705d470
Henry Post <henryfbp@gmail.com>
2020-07-26 13:37:21 -0500

    fix code with copy-paste ;)

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+    // TODO: remove the constructor
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
+        // TODO: use onClick={this.props.onClick}
+        // TODO: replace this.state.value with this.props.value
         return (
-            <button
-                className="square"
-                onClick={() => { this.props.onClick() }}
-            >
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({ value: 'X' })}>
+                {this.state.value}
             </button>
         );
     }
@@ -18,23 +25,12 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
-        }
-    }
-
-    handleClick(i) {
-        const squares = this.squares.state.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+            squares: Array(9).fill(null),
+        };
     }
 
     renderSquare(i) {
-        return (
-            <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
-            />
-        );
+        return <Square value={this.state.squares[i]} />;
     }
 
     render() {
@@ -44,13 +40,13 @@ class Board extends React.Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare(0)} {this.renderSquare(1)} {this.renderSquare(2)}
+                    {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3)} {this.renderSquare(4)} {this.renderSquare(5)}
+                    {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(6)} {this.renderSquare(7)} {this.renderSquare(8)}
+                    {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
                 </div>
             </div>
         );
