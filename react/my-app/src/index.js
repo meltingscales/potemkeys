e6db494c1a9de3b04ddfdafe0682a6107213ef7e
Henry Post <henryfbp@gmail.com>
2020-07-26 13:02:45 -0500

    make buttons interactive

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button
+                className="square"
+                onClick={() => { this.setState({ value: 'X' }) }}
+            >
+                {this.state.value}
             </button>
         );
     }
@@ -13,7 +25,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i} />;
     }
 
     render() {
