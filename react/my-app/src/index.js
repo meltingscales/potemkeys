88b9aef4f9a87d43e73ce6b557d90c02a734cdb6
Henry Post <henryfbp@gmail.com>
2020-07-26 20:14:00 -0500

    show winner

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -51,7 +51,16 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + this.state.currentMove;
+        const winner = calculateWinner(this.state.squares)
+
+        let status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = 'Next player: ' + this.state.currentMove;
+        }
 
         return (
             <div>
@@ -92,3 +101,24 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+// Return the winning object if a 3x3 array of objects has a winning tic tac toe move.
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
