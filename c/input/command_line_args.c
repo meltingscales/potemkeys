c3b343aaec1cf78f9c3322d1f756693cb22b6f46
Henry Post <HenryFBP@gmail.com>
2020-11-20 18:07:11 -0600

    print command line args

--- c/input/command_line_args.c
+++ c/input/command_line_args.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
+
+
+  printf("argc is %d\n",argc);
+  for (int i = 0; i < argc; i++)
+  {
+    printf("argument %d = %s\n",i,argv[i]);
+  }  
   
   if(argc < 3)
   {
