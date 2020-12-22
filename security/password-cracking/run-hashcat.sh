1946a3b800b07dc4f3c3da7d78977b0e93144743
Henry Post <henryfbp@gmail.com>
2020-12-22 15:58:49 -0600

    mention why -m 1420

--- security/password-cracking/run-hashcat.sh
+++ security/password-cracking/run-hashcat.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 hashcat -a 0 -m 1420 django-salted-sha256.hash example.dict -r best64.rule
+# We use mode 1420 here because the format from the database was 'hash(salt); hash(password)'
+# Run `man hashcat` for more info
+
 # --show 
 # --force
