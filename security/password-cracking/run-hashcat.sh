0e5c71d26e9f6481200352246ebc0da411cd98bd
Henry Post <henryfbp@gmail.com>
2020-12-22 16:17:38 -0600

    add more hashes and more examples

--- security/password-cracking/run-hashcat.sh
+++ security/password-cracking/run-hashcat.sh
@@ -4,5 +4,11 @@ hashcat -a 0 -m 1420 django-salted-sha256.hash example.dict -r best64.rule
 # We use mode 1420 here because the format from the database was 'hash(salt); hash(password)'
 # Run `man hashcat` for more info
 
+# Other options:
 # --show 
 # --force
+
+# Other examples:
+
+# Bruteforce (not using wordlists, this takes longer but is more thorough)
+# hashcat -a 3 -m 1420 django-salted-sha256.hash 
