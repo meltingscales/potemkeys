475334c5af5582b713d6b7af260205e50b871d56
Henry Post <henryfbp@gmail.com>
2020-12-22 16:19:28 -0600

    add way to rm potfile

--- security/password-cracking/run-hashcat.sh
+++ security/password-cracking/run-hashcat.sh
@@ -12,3 +12,6 @@ hashcat -a 0 -m 1420 django-salted-sha256.hash example.dict -r best64.rule
 
 # Bruteforce (not using wordlists, this takes longer but is more thorough)
 # hashcat -a 3 -m 1420 django-salted-sha256.hash 
+
+# Delete pot file:
+# rm ~/.hashcat/hashcat.potfile
