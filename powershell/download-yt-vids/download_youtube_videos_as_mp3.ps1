31368251eba395ee58a718cb330d676ffb98fdbd
Henry Post <henryfbp@gmail.com>
2021-06-12 12:26:06 -0500

    add -k to keep files

--- powershell/download-yt-vids/download_youtube_videos_as_mp3.ps1
+++ powershell/download-yt-vids/download_youtube_videos_as_mp3.ps1
@@ -2,7 +2,7 @@ get-content "videos.txt" | ForEach-Object {
 
     write-host "downloading" $_ " as mp3"
 
-    python -m youtube_dl -x --audio-format mp3 $_
+    python -m youtube_dl -x -k --audio-format mp3 $_
 
 }
 
