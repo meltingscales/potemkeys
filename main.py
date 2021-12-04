d49829f7aa9ab6fc1e5fc03f71c3d4871b82f27b
henryfbp <HenryFBP@gmail.com>
2021-12-04 11:30:28 -0500

    first release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # keylogger using pynput module
 
-# links that make me want to kill myself
+# links that make me want to `git commit -m 'fortnite battle royale'`
 # https://stackoverflow.com/questions/25381589/pygame-set-window-on-top-without-changing-its-position
 
 from typing import List
@@ -26,33 +26,12 @@ NOT_TOPMOST = -2
 
 TITLE = 'This app is for goldfish who can\'t remember buttons. Are you a goldfish? :3c'
 RUNNING = True
-MESSAGE=['phucking beesechurger']
+MESSAGE = ['hello :)']
 
 white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
 
-# class RECT(Structure):
-#     _fields_ = [
-#         ('left',    c_long),
-#         ('top',     c_long),
-#         ('right',   c_long),
-#         ('bottom',  c_long),
-#     ]
-#     def width(self):
-#         return self.right  - self.left
-#     def height(self):
-#         return self.bottom - self.top
-
-
-# def onTop(window:int): # https://stackoverflow.com/a/25383929/4262535
-#     '''
-#     ex:
-#         onTop(pygame.display.get_wm_info()['window'])
-#     '''
-#     rc = RECT()
-#     windll.user32.GetWindowRect(window, byref(rc))
-#     windll.user32.SetWindowPos(window, -1, rc.left, rc.top, 0, 0, 0x0001)
 
 def window_always_on_top(hwnd: int, x: int = 100, y: int = 200):
     user32 = ctypes.WinDLL("user32")
@@ -72,24 +51,43 @@ def window_always_on_top(hwnd: int, x: int = 100, y: int = 200):
 
 # keymaps
 ggst_map = {
+    'W': "jump",
     'A': "left",
+    'S': "crouch",
+    'D': "right",
+
+    'U': '[P] Punch',
+    'I': '[S] Slash',
+    'J': '[K] Kick',
+    'K': '[HS] HSlash',
+    'O': '[D] Dust'
 }
 
 tekken7map = {
-    'A': "left"
+    'W': "jump",
+    'A': "left",
+    'S': "crouch",
+    'D': "right",
 }
 
-game_map = {
+ALL_GAME_MAPS = {
     'GG:S': ggst_map,
     'Tekken 7': tekken7map,
 }
 
+# just use GG:S for now
+ACTIVE_GAME_MAP = ALL_GAME_MAPS['GG:S']
 
-def on_press(key,strref=MESSAGE):
+
+def on_press(key, strref=MESSAGE):
 
     try:
         print('alphanumeric key {0} pressed'.format(key.char))
-        strref[0]="NIGGA YOU PRESSING "+key.char+" ?????"
+        strref[0] = "{}".format(key.char)
+
+        normalized_key = key.char.upper()
+        if(normalized_key in ACTIVE_GAME_MAP.keys()):
+            strref[0] += '={}'.format(ACTIVE_GAME_MAP[normalized_key])
 
     except AttributeError:
         print('special key {0} pressed'.format(key))
@@ -103,19 +101,21 @@ def on_release(key):
     #     return False
 
 
-
 if __name__ == '__main__':
 
     # using .start() is non blocking
     keylistener = Listener(on_press=on_press, on_release=on_release)
     keylistener.start()
 
-    width, height = 400, 300
+    gamewidth, gameheight = 400, 300
+    screenwidth, screenheight = 1920, 1080
 
     pygame.init()
 
-    FONT = pygame.font.SysFont("Arial",32)
-    DISPLAYSURFACE = pygame.display.set_mode((width, height))
+    FONT = pygame.font.SysFont("Consolas", 32)
+    DISPLAYSURFACE = pygame.display.set_mode(
+        (gamewidth, gameheight), pygame.RESIZABLE
+    )
     pygame.display.set_caption(TITLE)
     # our int handle is pygame.display.get_wm_info()['window']
 
@@ -123,11 +123,10 @@ if __name__ == '__main__':
     window_handle: int = pygame.display.get_wm_info()['window']
     window_always_on_top(
         window_handle,
-        x=int((1920/2)-(width/2)),
-        # who cares????? so what, you can do math 😭😔
-        y=int((1080/2)-(height/2))
+        x=int((screenwidth/2)-(gamewidth/2)),
+        y=int((screenheight/2)-(gameheight/2))
     )
-    
+
     # main game loop
     while RUNNING:
 
@@ -139,7 +138,11 @@ if __name__ == '__main__':
 
         text = FONT.render(MESSAGE[0], True, green, blue)
         textRect = text.get_rect()
-        textRect.center = (width // 2, height // 2)
+        
+        gamewidth, gameheight = DISPLAYSURFACE.get_size()
+
+
+        textRect.center = (gamewidth // 2, gameheight // 2)
         DISPLAYSURFACE.fill(white)
         DISPLAYSURFACE.blit(text, textRect)
 
--- readme.md
+++ readme.md
@@ -6,15 +6,21 @@ For those who can't remember {keyboard input => controller} mappings, and want t
 
 ## How do I use it?
 
-You must use Windows currently. When I'm less lazy (never) I will make X11 equivalents for the win32 API code I use to make the window stay on top.
+You must use Windows currently. 
 
-1.  Install Python
+When I'm less lazy (never) I will make X11 equivalents for the win32 API code I use to make the window stay on top.
+
+1.  Install Python version 3.whatever
 2.  Clone this repo
-3.  Run:
+3.  In the repo's folder, run:
 
     ```
+    python -m pip install pipenv
     pipenv install
     pipenv shell
     python main.py
     ```
-4.  The window should stay on top. Play your game in borderless mode and see the inputs get transformed and shown to you.
+
+4.  The window should stay on top.
+
+    Play your game in borderless mode and see the inputs get transformed and shown to you.
