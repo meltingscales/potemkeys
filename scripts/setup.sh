6cf86a88336a89060e5257b18d1d7fcf51d80867
Henry Post <HenryFBP@gmail.com>
2022-02-09 10:34:22 -0500

    add macos release

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+  platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+  platform='freebsd'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  platform='darwin'
+fi
+
 echo "Make sure poetry exists..."
 
 which poetry
 # if exit code is nonzero, it is not a command.
 if [ "$?" -eq "1" ]; then
-  python -m pip install poetry
+  python3 -m pip install poetry
 fi
 
-echo "Making sure required tools are installed."
-sudo apt-get install -y xdotool wmctrl
+if [[ $platform == 'linux' ]]; then
+  echo "Making sure required tools are installed."
+  sudo apt-get install -y xdotool wmctrl
+else
+  echo "Not Linux, maybe OSX? Cannot install xdotool or wmctrl. Skipping."
+fi
 
 poetry install
 
